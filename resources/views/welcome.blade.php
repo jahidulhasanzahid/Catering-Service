fa3c08e2075970bda2667f2ddaee574e506b4945
jahidulhasanzahid <2017zahidhasan@gmail.com>
2019-10-01 11:37:15 +0600

    Update welcome.blade.php
    Title and Heading Change.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Multi Auth
+                    Multi User Login System.
                 </div>
             </div>
         </div>
